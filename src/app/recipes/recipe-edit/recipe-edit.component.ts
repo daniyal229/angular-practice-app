cc47ad07a5ff63b249d5620e7dbc7327bc456a30
Daniyal Shah <daniyal.shah@arkhitech.com>
2018-06-26 21:02:48 -0700

    forms improved

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { NgForm } from '@angular/forms';
 import { Recipe } from '../../models/recipe.model';
 import { RecipeListService } from '../../services/recipe-list.service';
 import $ from 'jquery';
@@ -12,24 +13,28 @@ import $ from 'jquery';
 
 export class RecipeEditComponent implements OnInit {
 
+  @ViewChild('recipeForm') recipeForm: NgForm;
+
   isNewRecipe: boolean = false;
   recipe: Recipe;
   constructor(private route: ActivatedRoute, private RecipeListService: RecipeListService) { }
 
   ngOnInit() {
+    debugger
     this.isNewRecipe = (this.route.snapshot.routeConfig.path === 'new');
     if (!!!this.isNewRecipe) {
       this.route.params.subscribe(
         (params: Params) => {
-          this.recipe = $.extend({},this.RecipeListService.getRecipe(+params['id']));
+          this.recipe = this.RecipeListService.getRecipe(+params['id']);
+          this.recipeForm.value.recipe = this.recipe;
         }
       );
     } else {
-       this.recipe = new Recipe(Math.floor((Math.random() * 100000) + 1);,'','','',[]);
+       this.recipe = new Recipe(Math.floor((Math.random() * 100000) + 1),'','','',[]);
     }
   }
 
-  addOrUpdateRecipe(recipe){
-    this.RecipeListService.addRecipe(recipe);
+  addOrUpdateRecipe(recipeForm: NgForm){
+    debugger
   }
 }
