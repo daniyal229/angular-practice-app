34c0bc21629562e2ce022897c496e7965174caa5
Syed Daniyal Shah <daniyal.shah@softrove.com>
2018-09-24 18:22:09 +0500

    initial commit for forexviewer react native application

---
+++ daniyal/ForexViewer/src/index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import AppComponent from './app.component';
+import {name as appName} from './config.json';
+
+AppRegistry.registerComponent(appName, () => AppComponent);
